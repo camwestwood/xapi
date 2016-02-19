040480025dbe28d851a73b1676033b0e318a7c14
Gary Gilbert <gjgilbert@gmail.com>
2016-02-19 16:58:38 -0500

    added ht2 comments/suggestions

--- lap/apereo/model_output.js
+++ lap/apereo/model_output.js
@@ -14,46 +14,57 @@
 	        "en-US": "receive"
 	    }
     },
-    "context": {
-      "contextActivities": {
-        "grouping": [
-            {
-                "objectType": "Activity",
-                "definition": {
-                    "type": "https://github.com/jiscdev/analytics-udd/blob/master/predictive-core.md#mod_instance_id",
-                    "description": {
-                        "en": "Jisc Module Instance Id"
-                    },
-                    "name": {
-                        "en": "Jisc Module Instance Id"
-                    }
-                },
-                "id": "78910"
-            }
-        ]
-      }
-    },
-    "timestamp": "2016-02-10T10:20:28+00:00",
     "object": {
-        "objectType": "Activity",
-	    "id": "https://lap.jisc.ac.uk/earlyAlert",
-	    "definition": {
-	        "type": "http://activitystrea.ms/schema/1.0/alert",
-	        "name": {
-	            "en-GB": "An early alert",
-	            "en-US": "An early alert"
-	        },
-	        "description": {
-	            "en-GB": "An early alert",
-	            "en-US": "An early alert"
-	        }
+	  "objectType": "Activity",
+	  "id": "https://lap.jisc.ac.uk/earlyAlert/unicon/id",
+	  "definition": {
+	    "type": "http://activitystrea.ms/schema/1.0/alert",
+	    "name": {
+	      "en-GB": "An early alert",
+	      "en-US": "An early alert"
+	    },
+	    "description": {
+	      "en-GB": "An early alert",
+	      "en-US": "An early alert"
+	    },
+	    "moreInfo": "url to view the alert",
+	    "extensions": {
+	      "https://lap.jisc.ac.uk/earlyAlert/type": "UNICON"
 	    }
-    },
+	  }
+	},
+	"context": {
+	  "contextActivities": {
+	    "grouping": [
+	      {
+	        "objectType": "Activity",
+	        "id": "https://github.com/jiscdev/analytics-udd/blob/master/predictive-core.md#mod_instance/id",
+	        "definition": {
+	          "type": "http://adlnet.gov/expapi/activities/module",
+	          "description": {
+	              "en": "Jisc Module Instance"
+	          },
+	          "name": {
+	              "en": "Jisc Module Instance"
+	          },
+	          "extensions": {
+	            "https://lap.jisc.ac.uk/taxonomy": "MOD_INSTANCE"
+	          }
+	        }
+	      }
+	    ]
+	  }
+	},
+    "timestamp": "2016-02-10T10:20:28+00:00",
     "result": {
-        "completion": true,
         "extensions": {
-          "https://lap.jisc.ac.uk/earlyAlert/score": "HIGH"
+          "https://lap.jisc.ac.uk/earlyAlert/score": "HIGH",
+          "https://lap.jisc.ac.uk/earlyAlert/metricN": "3.89"
         }
-      }
-
+      },
+      "authority": {
+	    "objectType": "Agent",
+	    "name": "Unicon",
+	    "mbox": "mailto:hello@unicon.net"
+	 }
 }
